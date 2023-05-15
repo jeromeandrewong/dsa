b84da156fb578fd5829bc60f35025c8910b2867f
Jerome Wong <jeromewongjh@gmail.com>
2023-05-15 17:49:55 +0800

    feat: two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const jumpDist = Math.floor(Math.sqrt(breaks.length));
+    let currentFloor = jumpDist;
+    for (; currentFloor < breaks.length; currentFloor += jumpDist) {
+        //check for breaks
+        if (breaks[currentFloor]) {
+            break;
+        }
+    }
+    //jump back by a sqrt and linearly walk up
+    currentFloor -= jumpDist;
+    for (
+        let j = 0;
+        j <= jumpDist && currentFloor < breaks.length;
+        ++j, ++currentFloor
+    ) {
+        if (breaks[currentFloor]) {
+            return currentFloor;
+        }
+    }
+    return -1;
 }
+``;
