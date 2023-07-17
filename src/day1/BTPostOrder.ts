2e68d40558beac24c5deeeb35783ed8ba0c4ecae
Jerome Wong <jeromewongjh@gmail.com>
2023-07-17 21:55:23 +0800

    feat: tree traversals

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,22 @@
 export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
+}
 
+// visit node after recursion, root at the end
+// good for freeing up memory
+
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
+    // recurse
+    // pre
+
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // post
+    path.push(curr.value);
+    return path;
 }
--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,20 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
-
+    return walk(head, []);
 }
+
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
+    // recurse
+    // pre
+    path.push(curr.value);
+
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // post
+    return path;
+}
+// first visit node then do recursion (hence the name preorder); root at the start
